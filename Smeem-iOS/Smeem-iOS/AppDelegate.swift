c23ef5e5913031b9334eef155ea34cc7f99121fc
cchanmi <gkfn980@hs.ac.kr>
2023-04-26 01:20:04 +0900

    firebase library, push setting

--- Smeem-iOS/Smeem-iOS/AppDelegate.swift
+++ Smeem-iOS/Smeem-iOS/AppDelegate.swift
@@ -7,14 +7,57 @@
 
 import UIKit
 
+import FirebaseCore
+import FirebaseMessaging
+
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
+        // 메시지 대리자 설정
+        Messaging.messaging().delegate = self
+        
+        UNUserNotificationCenter.current().delegate = self
+
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter
+          .current()
+          .requestAuthorization(
+            options: authOptions,completionHandler: { (_, _) in }
+          )
+        
+        // APNs 등록
+        application.registerForRemoteNotifications()
         return true
+    }
+    
+    // deviceToken을 할당하여 Firebase Messaging 서버에 토큰 등록
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    // 현재 토큰 가져오기
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        // 토큰 갱신 모니터링
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
+        
+        // 등록된 토큰 가져오기
+        Messaging.messaging().token { token, error in
+          if let error = error {
+            print("Error fetching FCM registration token: \(error)")
+          } else if let token = token {
+            print("FCM registration token: \(token)")
+          }
+        }
     }
 
     // MARK: UISceneSession Lifecycle
