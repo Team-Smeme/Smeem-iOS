e8a1f8501e9cb16b03194067850e4c87f15c88a9
Joon Baek <joonbaek12@gmail.com>
2023-05-02 14:59:48 +0900

    [Setting] #4 - ColorLiterals, FontLiterals 추가

--- Smeem-iOS/Smeem-iOS/Global/Literals/FontLiterals.swift
+++ Smeem-iOS/Smeem-iOS/Global/Literals/FontLiterals.swift
@@ -5,4 +5,80 @@
 //  Created by Joon Baek on 2023/05/01.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+    @nonobjc class var h1: UIFont {
+        return UIFont.font(.pretendardBold, ofSize: 26)
+    }
+    
+    @nonobjc class var h2: UIFont {
+        return UIFont.font(.pretendardBold, ofSize: 24)
+    }
+    
+    @nonobjc class var h3: UIFont {
+        return UIFont.font(.pretendardBold, ofSize: 20)
+    }
+    
+    @nonobjc class var s1: UIFont {
+        return UIFont.font(.pretendardBold, ofSize: 18)
+    }
+    
+    @nonobjc class var s2: UIFont {
+        return UIFont.font(.pretendardSemiBold, ofSize: 18)
+    }
+    
+    @nonobjc class var s3: UIFont {
+        return UIFont.font(.pretendardMedium, ofSize: 18)
+    }
+    
+    @nonobjc class var s4: UIFont {
+        return UIFont.font(.pretendardRegular, ofSize: 18)
+    }
+    
+    @nonobjc class var b1: UIFont {
+        return UIFont.font(.pretendardBold, ofSize: 16)
+    }
+    
+    @nonobjc class var b2: UIFont {
+        return UIFont.font(.pretendardSemiBold, ofSize: 16)
+    }
+    
+    @nonobjc class var b3: UIFont {
+        return UIFont.font(.pretendardMedium, ofSize: 16)
+    }
+    
+    @nonobjc class var b4: UIFont {
+        return UIFont.font(.pretendardRegular, ofSize: 16)
+    }
+    
+    @nonobjc class var c1: UIFont {
+        return UIFont.font(.pretendardBold, ofSize: 14)
+    }
+    
+    @nonobjc class var c2: UIFont {
+        return UIFont.font(.pretendardMedium, ofSize: 14)
+    }
+    
+    @nonobjc class var c3: UIFont {
+        return UIFont.font(.pretendardRegular, ofSize: 14)
+    }
+    
+    @nonobjc class var c4: UIFont {
+        return UIFont.font(.pretendardRegular, ofSize: 12)
+    }
+}
+
+enum FontName: String {
+    case pretendardBold = "Pretendard-Bold"
+    case pretendardSemiBold = "Pretendard-SemiBold"
+    case pretendardMedium = "Pretendard-Medium"
+    case pretendardRegular = "Pretendard-Regular"
+}
+
+extension UIFont {
+    static func font(_ style: FontName, ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: style.rawValue, size: size)!
+    }
+}
+
