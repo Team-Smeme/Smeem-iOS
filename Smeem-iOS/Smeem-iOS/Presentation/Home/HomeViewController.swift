e87a0a22592fd89983487556904e527dbf57ded7
임주민 <limj1213@gmail.com>
2023-05-05 16:42:25 +0900

    [Chore] #9 - 캘린더 작업을 위한 뷰컨트롤러 파일 추가

---
+++ Smeem-iOS/Smeem-iOS/Presentation/Home/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  Smeem-iOS
+//
+//  Created by 임주민 on 2023/05/05.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
