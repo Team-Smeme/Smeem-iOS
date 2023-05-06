36a6179e5d5ba6bf842533d94ae8450a9758e1ce
cchanmi <gkfn980@hs.ac.kr>
2023-05-07 02:06:12 +0900

    [Feat] #12 - viewType에 따른 bottomSheetView 높이 분기 처리

---
+++ Smeem-iOS/Smeem-iOS/Presentation/BottomSheet/BottomSheetViewController.swift
@@ -0,0 +1,79 @@
+//
+//  BottomSheetViewController.swift
+//  Smeem-iOS
+//
+//  Created by 황찬미 on 2023/05/06.
+//
+
+import UIKit
+
+import SnapKit
+
+final class BottomSheetViewController: UIViewController {
+    
+    var defaultLoginHeight: CGFloat = 282
+    var defaultSignUpHeight: CGFloat = 394
+    
+    // MARK: - Property
+    
+    // MARK: - UI Property
+    
+    private lazy var dimmedView: UIView = {
+        let view = UIView()
+        view.layer.backgroundColor = UIColor(red: 0.09, green: 0.09, blue: 0.086, alpha: 0.65).cgColor
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dimmedViewDidTap)))
+        return view
+    }()
+    
+    lazy var bottomSheetView: BottomSheetView = {
+        let view = BottomSheetView()
+        view.backgroundColor = .white
+        view.viewType = .login
+        return view
+    }()
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setBackgroundColor()
+        setLayout()
+    }
+    
+    // MARK: - @objc
+    
+    @objc func dimmedViewDidTap() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.bottomSheetView.frame.origin.y = self.view.frame.height
+        }) { (completion) in
+            self.dismiss(animated: false, completion: nil)
+        }
+    }
+    
+    // MARK: - Custom Method
+    
+    private func setBackgroundColor() {
+        view.backgroundColor = .clear
+    }
+    
+    private func setLayout() {
+        view.addSubviews(dimmedView, bottomSheetView)
+        
+        dimmedView.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        if bottomSheetView.viewType == .login {
+            bottomSheetView.snp.makeConstraints {
+                $0.height.equalTo(defaultLoginHeight)
+                $0.leading.trailing.bottom.equalToSuperview()
+            }
+        } else {
+            bottomSheetView.snp.makeConstraints {
+                $0.height.equalTo(defaultSignUpHeight)
+                $0.leading.trailing.bottom.equalToSuperview()
+            }
+        }
+    }
+}
