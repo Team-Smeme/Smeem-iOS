99f91a1e2a00994036288f741f4de72cd625a39b
cchanmi <gkfn980@hs.ac.kr>
2023-05-16 20:18:58 +0900

    [Feat] #14 - 학습 권장 방법 UI 구현

---
+++ Smeem-iOS/Smeem-iOS/Presentation/Onboarding/How/HowOnboardingViewController.swift
@@ -0,0 +1,130 @@
+//
+//  HowOnboardingViewController.swift
+//  Smeem-iOS
+//
+//  Created by 황찬미 on 2023/05/16.
+//
+
+import UIKit
+
+final class HowOnboardingViewController: UIViewController {
+    
+    // MARK: - Property
+    
+    // MARK: - UI Property
+    
+    private let nowStepOneLabel: UILabel = {
+        let label = UILabel()
+        label.text = "2"
+        label.font = .s1
+        label.setTextWithLineHeight(lineHeight: 21)
+        label.textColor = .point
+        return label
+    }()
+    
+    private let divisionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "/"
+        label.font = .c3
+        label.textColor = .black
+        return label
+    }()
+    
+    private let totalStepLabel: UILabel = {
+        let label = UILabel()
+        label.text = "3"
+        label.font = .c3
+        label.textColor = .black
+        return label
+    }()
+    
+    private let titleLearningLabel: UILabel = {
+        let label = UILabel()
+        label.text = "학습 권장 방법"
+        label.font = .h2
+        label.textColor = .black
+        return label
+    }()
+    
+    private let detailLearningLabel: UILabel = {
+        let label = UILabel()
+        label.text = "스밈과 이만큼 함께라면 목표를 이룰 수 있어요."
+        label.font = .b4
+        label.textColor = .black
+        return label
+    }()
+    
+    private let learningLabelStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .leading
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 6
+        return stackView
+    }()
+    
+    private let howLearningView: HowLearningView = {
+        let view = HowLearningView()
+        view.buttontype = .logo
+        return view
+    }()
+    
+    private let nextButton: SmeemButton = {
+        let button = SmeemButton()
+        button.setTitle("다음", for: .normal)
+        return button
+    }()
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setBackgroundColor()
+        setLayout()
+    }
+    
+    // MARK: - @objc
+    
+    // MARK: - Custom Method
+    
+    private func setBackgroundColor() {
+        view.backgroundColor = .white
+    }
+    
+    private func setLayout() {
+        view.addSubviews(nowStepOneLabel, divisionLabel, totalStepLabel,                              learningLabelStackView, howLearningView, nextButton)
+        learningLabelStackView.addArrangedSubviews(titleLearningLabel, detailLearningLabel)
+        
+        nowStepOneLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(26)
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(36)
+        }
+        
+        divisionLabel.snp.makeConstraints {
+            $0.leading.equalTo(nowStepOneLabel.snp.trailing).offset(2)
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(40)
+        }
+        
+        totalStepLabel.snp.makeConstraints {
+            $0.leading.equalTo(divisionLabel.snp.trailing).offset(1)
+            $0.top.equalTo(divisionLabel.snp.top)
+        }
+        
+        learningLabelStackView.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(26)
+            $0.top.equalTo(totalStepLabel.snp.bottom).offset(19)
+        }
+        
+        howLearningView.snp.makeConstraints {
+            $0.top.equalTo(learningLabelStackView.snp.bottom).offset(28)
+            $0.centerX.equalToSuperview()
+        }
+        
+        nextButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(18)
+            $0.bottom.equalToSuperview().inset(50)
+            $0.height.equalTo(convertByHeightRatio(60))
+        }
+    }
+}
