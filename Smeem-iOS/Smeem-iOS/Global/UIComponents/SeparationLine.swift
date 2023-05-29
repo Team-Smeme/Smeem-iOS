10e8a149f7d471e7afa3e67d89a2849ad1181b14
Joon Baek <joonbaek12@gmail.com>
2023-05-30 00:53:56 +0900

    [Chore] #18 - mediumLine 속성 추가 및 사용방법 문서화

--- Smeem-iOS/Smeem-iOS/Global/UIComponents/SeparationLine.swift
+++ Smeem-iOS/Smeem-iOS/Global/UIComponents/SeparationLine.swift
@@ -5,39 +5,33 @@
 //  Created by Joon Baek on 2023/05/02.
 //
 
-//import UIKit
-//
-//import SnapKit
-//
-//enum LineHeight: CGFloat {
-//    case thin = 1
-//    case thick = 6
-//}
-//
-//class SeparationLine: UIView {
-//    var height: LineHeight
-//
-//    convenience init(height: LineHeight) {
-//        self.init(height: height)
-//        self.height = height
-//        self.backgroundColor = height == .thin ? UIColor.gray100 : UIColor.gray200
-//
-//        snp.makeConstraints { make in
-//            make.height.equalTo(height.rawValue)
-//        }
-//    }
-//}
+/**
+ 1. 사용할 VC에서 SeparationLine 프로퍼티 생성
+ pivate let mediumLine = SeparationLine(height: .medium)
+ 
+ 2. view에 addSubView 하고 y축(top)값만 설정
+ **/
 
 import UIKit
+
 import SnapKit
 
 class SeparationLine: UIView {
+    
+    // MARK: - UI Property
+    
     enum LineHeight: CGFloat {
         case thin = 1
+        case medium = 2
         case thick = 6
     }
     
-    var height: LineHeight
+    private let thinLineColor = UIColor.gray100
+    private let thickLineColor = UIColor.gray200
+    
+    private let height: LineHeight
+    
+    // MARK: - Life Cycle
     
     convenience init() {
         self.init(height: .thin)
@@ -46,16 +40,29 @@ class SeparationLine: UIView {
     init(height: LineHeight) {
         self.height = height
         super.init(frame: .zero)
-        self.backgroundColor = height == .thin ? UIColor.gray100 : UIColor.gray200
-        
-        snp.makeConstraints {
-            $0.height.equalTo(height.rawValue)
-        }
+        setColors()
+        setLayout()
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - Custom method
+    
+    private func setColors() {
+        switch height {
+        case .thin, .medium:
+            self.backgroundColor = thinLineColor
+        case .thick:
+            self.backgroundColor = thickLineColor
+        }
+    }
+    
+    private func setLayout() {
+        snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(height.rawValue)
+        }
+    }
 }
-
-
