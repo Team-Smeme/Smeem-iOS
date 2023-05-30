2419c2bd967819e6f6a8f391ac42781f8faa4229
Joon Baek <joonbaek12@gmail.com>
2023-05-31 02:02:08 +0900

    [Chore] #18 - CustomToastView 사용방법 문서화 및 MARK주석 추가

--- Smeem-iOS/Smeem-iOS/Global/UIComponents/CustomToastView.swift
+++ Smeem-iOS/Smeem-iOS/Global/UIComponents/CustomToastView.swift
@@ -5,22 +5,36 @@
 //  Created by Joon Baek on 2023/05/03.
 //
 
+/**
+ 1. 사용할 VC에서 CustomToastView 생성
+ let toastView = CustomToastView(text: "토스트 메시지 텍스트")
+ 
+ 2. view에 addSubView 후 y축(bottom) 레이아웃 값만 입력해서 사용
+**/
+
 import UIKit
 
 import SnapKit
 
 class CustomToastView: UIView {
     
+    // MARK: - Property
+    
     private let text: String
+    
+    // MARK: - UI Property
     
     private let label: UILabel = {
         let label = UILabel()
         label.font = .c2
         label.textColor = .smeemWhite
+        //TODO: LineHeight 속성지정?
         label.setTextWithLineHeight(lineHeight: 17)
         label.sizeToFit()
         return label
     }()
+    
+    // MARK: - Life Cycle
     
     public init(text: String) {
         self.text = text
@@ -33,7 +47,9 @@ class CustomToastView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    public func show(duration: TimeInterval, completion: (() -> Void)? = nil) {
+    // MARK: - Custom Method
+    
+    public func show(duration: TimeInterval) {
         UIView.animate(withDuration: 0.3, animations: {
             self.alpha = 1
         })
@@ -41,12 +57,11 @@ class CustomToastView: UIView {
             self.alpha = 0
         }) { _ in
             self.removeFromSuperview()
-            completion?()
         }
     }
-}
-
-extension CustomToastView {
+    
+    // MARK: - Layout
+    
     private func setToastViewUI() {
         backgroundColor = .toastBackground
         clipsToBounds = true
@@ -59,12 +74,13 @@ extension CustomToastView {
         
         label.snp.makeConstraints {
             $0.centerY.equalToSuperview()
-            $0.leading.equalToSuperview().offset(16)
+            $0.leading.equalToSuperview().offset(convertByWidthRatio(16))
         }
         
         snp.makeConstraints {
-            $0.width.equalTo(339)
-            $0.height.equalTo(50)
+            $0.centerX.equalTo(self)
+            $0.width.equalTo(convertByWidthRatio(339))
+            $0.height.equalTo(convertByHeightRatio(50))
         }
     }
 }
