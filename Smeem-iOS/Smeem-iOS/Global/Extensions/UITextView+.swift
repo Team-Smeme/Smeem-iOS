5b8c5c7347a559ee764b04a6f777a1c6e738bc61
Joon Baek <joonbaek12@gmail.com>
2023-05-31 02:27:01 +0900

    [Merge] #18 - Pull Develop

--- Smeem-iOS/Smeem-iOS/Global/Extensions/UITextView+.swift
+++ Smeem-iOS/Smeem-iOS/Global/Extensions/UITextView+.swift
@@ -2,16 +2,15 @@
 //  UITextView+.swift
 //  Smeem-iOS
 //
-//  Created by Joon Baek on 2023/05/10.
+//  Created by 황찬미 on 2023/05/20.
 //
 
 import UIKit
 
 extension UITextView {
-    
     func setLineSpacing() {
         let style = NSMutableParagraphStyle()
-        style.lineSpacing = 2
+        style.lineSpacing = 2.5
         let attributes = [
             NSAttributedString.Key.paragraphStyle: style,
             NSAttributedString.Key.font: UIFont.b4
