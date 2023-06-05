cf9a3a70e9dcf972a01ef70e2bd4f7081bfe7af6
Jonghwa Won <jongwha95@gmail.com>
2023-06-05 20:35:05 +0900

    [Add] #47 - 아이콘 상수화

--- Smeem-iOS/Smeem-iOS/Global/Literals/ImageLiterals.swift
+++ Smeem-iOS/Smeem-iOS/Global/Literals/ImageLiterals.swift
@@ -5,4 +5,23 @@
 //  Created by Joon Baek on 2023/05/01.
 //
 
-import Foundation
+import UIKit
+
+struct Constant {
+    private init() {}
+}
+
+extension Constant {
+    enum image {
+        
+        static let icnBack = UIImage(named: "icnBack")
+        static let icnCancelGrey = UIImage(named: "icnCancelGrey")
+        static let icnCancelSmall = UIImage(named: "icnCancelSmall")
+        static let icnCancelWhite = UIImage(named: "icnCancelWhite")
+        static let icnCheck = UIImage(named: "icnCheck")
+        static let icnCheckInactive = UIImage(named: "icnCheckInactive")
+        static let icnMore = UIImage(named: "icnMore")
+        static let icnMoreHoriz = UIImage(named: "icnMoreHoriz")
+        static let incCheckActive = UIImage(named: "incCheckActive")
+    }
+}
