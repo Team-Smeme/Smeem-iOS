526f94192f1e0d0e086d3692d93eaa91cbd133cf
Jonghwa Won <jongwha95@gmail.com>
2023-06-07 17:11:47 +0900

    [Feat] #56 - BadgeViewController 생성

---
+++ Smeem-iOS/Smeem-iOS/Presentation/MyPage/BadgeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BadgeViewController.swift
+//  Smeem-iOS
+//
+//  Created by JH on 2023/06/07.
+//
+
+import UIKit
+
+import Kingfisher
+import SnapKit
+
+class BadgeViewController: UIViewController {
+    
+    // MARK: - Property
+    
+    // MARK: - UI Property
+    
+    // MARK: - Life Cycle
+    
+    // MARK: - @objc
+    
+    // MARK: - Custom Method
+    
+    // MARK: - Layout
+    
+}
+
+// MARK: - UITableView Delegate
