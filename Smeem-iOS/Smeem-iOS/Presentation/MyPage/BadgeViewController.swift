cb2350497b3eed947d21d55a2b3a99fb76156f5b
Jonghwa Won <jongwha95@gmail.com>
2023-06-07 17:27:09 +0900

    [Feat] #56 - 헤더부 UI 구현

--- Smeem-iOS/Smeem-iOS/Presentation/MyPage/BadgeViewController.swift
+++ Smeem-iOS/Smeem-iOS/Presentation/MyPage/BadgeViewController.swift
@@ -16,7 +16,26 @@ class BadgeViewController: UIViewController {
     
     // MARK: - UI Property
     
+    private let headerContainerView = UIView()
+    
+    private let cancelButton: UIButton = {
+        let button = UIButton()
+        button.setImage(Constant.Image.icnCancelBlack, for: .normal)
+        return button
+    }()
+    // TODO: addTarget 넣기
+
+    
     // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setBackgroundColor()
+        setLayout()
+        hiddenNavigationBar()
+    }
+
     
     // MARK: - @objc
     
@@ -24,6 +43,25 @@ class BadgeViewController: UIViewController {
     
     // MARK: - Layout
     
+    private func setBackgroundColor() {
+        view.backgroundColor = .smeemWhite
+    }
+    
+    private func setLayout() {
+        view.addSubviews(headerContainerView)
+        headerContainerView.addSubview(cancelButton)
+        
+        headerContainerView.snp.makeConstraints{
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(convertByHeightRatio(66))
+        }
+        
+        cancelButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(10)
+            $0.height.equalTo(45)
+        }
+    }
 }
 
 // MARK: - UITableView Delegate
