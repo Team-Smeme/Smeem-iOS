fedb3bd95ea82ad0e9531500c9675c19ed86de22
cchanmi <gkfn980@hs.ac.kr>
2023-06-15 01:40:12 +0900

    [Feat] #51 - 영어로 일기 작성하기 튜토리얼 구현

--- Smeem-iOS/Smeem-iOS/Presentation/ForeignDiary/StepTwoKoreanDiaryViewController.swift
+++ Smeem-iOS/Smeem-iOS/Presentation/ForeignDiary/StepTwoKoreanDiaryViewController.swift
@@ -33,15 +33,33 @@ final class StepTwoKoreanDiaryViewController: DiaryViewController {
         return button
     }()
     
+    private var tutorialImageView: UIImageView? = {
+        let imageView = UIImageView()
+        imageView.image = Constant.Image.tutorialDiaryStepTwo
+        return imageView
+    }()
+    
+    private lazy var dismissButton: UIButton? = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(dismissButtonDidTap), for: .touchUpInside)
+        return button
+    }()
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setLayout()
+        checkToturial()
     }
     
     // MARK: - @objc
+
+    @objc override func dismissButtonDidTap() {
+        tutorialImageView?.removeFromSuperview()
+        dismissButton?.removeFromSuperview()
+    }
     
     // MARK: - Custom Method
     
@@ -73,6 +91,28 @@ final class StepTwoKoreanDiaryViewController: DiaryViewController {
             $0.leading.equalTo(placeHolderLabel)
             $0.width.equalTo(convertByWidthRatio(92))
             $0.height.equalTo(convertByHeightRatio(29))
+        }
+    }
+    
+    private func checkToturial() {
+        let tutorialDiaryStepTwo = UserDefaultsManager.tutorialDiaryStepTwo
+        
+        if !tutorialDiaryStepTwo {
+            UserDefaultsManager.tutorialDiaryStepTwo = true
+            
+            view.addSubviews(tutorialImageView ?? UIImageView(), dismissButton ?? UIButton())
+            
+            tutorialImageView?.snp.makeConstraints {
+                $0.top.leading.trailing.bottom.equalToSuperview()
+            }
+            dismissButton?.snp.makeConstraints {
+                $0.top.equalToSuperview().inset(convertByHeightRatio(371))
+                $0.trailing.equalToSuperview().inset(convertByHeightRatio(10))
+                $0.width.height.equalTo(convertByHeightRatio(45))
+            }
+        } else {
+            tutorialImageView = nil
+            dismissButton = nil
         }
     }
 }
