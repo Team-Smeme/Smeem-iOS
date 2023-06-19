0bd7d6d19cd46c320d2c49543e39d5b3a07b70cc
Joon Baek <joonbaek12@gmail.com>
2023-06-20 01:06:52 +0900

    [Feat] #57 - ForeignDiaryViewController에서 토스트메시지 메시지 실행

--- Smeem-iOS/Smeem-iOS/Presentation/ForeignDiary/ForeignDiaryViewController.swift
+++ Smeem-iOS/Smeem-iOS/Presentation/ForeignDiary/ForeignDiaryViewController.swift
@@ -11,5 +11,24 @@ final class ForeignDiaryViewController: DiaryViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        handleRightNavitationButton()
+    }
+    
+    private func handleRightNavitationButton() {
+        rightNavigationButton.addTarget(self, action: #selector(rightNavigationButtonDidTap), for: .touchUpInside)
+    }
+    
+    override func rightNavigationButtonDidTap() {
+        if rightNavigationButton.titleLabel?.textColor == .point {
+            //TODO: HomeView로 돌아가는 코드
+        } else {
+            regExToastView.show(duration: 0.7)
+            
+            view.addSubview(regExToastView)
+            regExToastView.snp.makeConstraints {
+                $0.centerX.equalToSuperview()
+                $0.bottom.equalTo(bottomView.snp.top).offset(-20)
+            }
+        }
     }
 }
