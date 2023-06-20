15106fd6d195ca7f90eab7b52085bb505f0ec442
Joon Baek <joonbaek12@gmail.com>
2023-06-20 14:34:13 +0900

    [Chore] #57 - CustomToastView->SmeemToastView로 이름 변경 및 코드 가독성 개선

--- Smeem-iOS/Smeem-iOS/Global/UIComponents/SmeemToastView.swift
+++ Smeem-iOS/Smeem-iOS/Global/UIComponents/SmeemToastView.swift
@@ -1,18 +1,18 @@
 //
-//  CustomToastView.swift
+//  SmeemToastView.swift
 //  Smeem-iOS
 //
 //  Created by Joon Baek on 2023/05/03.
 //
 
 /**
- 1. 사용할 VC에서 CustomToastView 생성
- let toastView = CustomToastView(text: "토스트 메시지 텍스트")
+ 1. 사용할 VC에서 SmeemToastView 생성
+ let toastView = SmeemToastView(text: "토스트 메시지 텍스트")
  
  2. view에 addSubView 후 y축(bottom) 레이아웃 값만 입력해서 사용
  
  3. duration에 띄워지는 시간을 입력해서 호출 toastView.show(duration: )
-**/
+ **/
 
 import UIKit
 
@@ -21,26 +21,43 @@ import SnapKit
 enum ToastViewType {
     case defaultToast(bodyType: BodyType)
     case errorToast(errorType: ErrorType)
+    
+    var displayText: (head: String?, body: String) {
+        switch self {
+        case .defaultToast(bodyType: let bodyType):
+            return (nil, bodyType.displayText)
+        case .errorToast(errorType: let errorType):
+            return (errorType.displayText, BodyType.error.displayText)
+        }
+    }
 }
 
 enum ErrorType: String {
     case networkError = "인터넷 연결을 확인해 주세요 :("
     case systemError = "죄송합니다, 시스템 오류가 발생했어요 :("
     case loadDataError = "데이터를 불러올 수 없어요 :("
+    
+    var displayText: String {
+        return self.rawValue
+    }
 }
 
 enum BodyType: String {
     case error = "재접속하거나 나중에 다시 시도해 주세요."
-    case regEx = "외국어를 포함해 작성해 주세요"
+    case regEx = "외국어를 포함해 작성해 주세요 :("
     case completed = "작성 완료"
     case changed = "변경 완료"
     case edited = "첨삭 완료"
+    
+    var displayText: String {
+        return self.rawValue
+    }
 }
 
-final class CustomToastView: UIView {
+final class SmeemToastView: UIView {
     
     // MARK: - Property
-
+    
     private let type: ToastViewType
     private let bodyText: String
     
@@ -92,7 +109,7 @@ final class CustomToastView: UIView {
     
     // MARK: - Custom Method
     
-    public func show(duration: TimeInterval) {
+    public func show(duration: TimeInterval = 0.7) {
         UIView.animate(withDuration: 0.3, animations: {
             self.alpha = 1
         })
@@ -106,21 +123,22 @@ final class CustomToastView: UIView {
     // MARK: - Layout
     
     private func setToastViewUI() {
+        let (headText, bodyText) = type.displayText
+        headLabel.text = headText
+        bodyLabel.text = bodyText
         
         switch type {
         case .defaultToast:
             bodyLabel.font = .c2
-            bodyLabel.text = bodyText
-        case .errorToast(let errorType):
+        case .errorToast:
             bodyLabel.font = .c4
-            headLabel.text = errorType.rawValue
-            bodyLabel.text = BodyType.error.rawValue
         }
         
         backgroundColor = .toastBackground
         clipsToBounds = true
         layer.cornerRadius = 6
     }
+    
     
     private func setToastViewLayout() {
         addSubviews(cautionImage, headLabel, bodyLabel)
