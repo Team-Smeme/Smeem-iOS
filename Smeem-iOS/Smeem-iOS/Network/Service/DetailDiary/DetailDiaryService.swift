63fed4ea3c744fb9544024e48a28f06c69e4fe09
Joon Baek <joonbaek12@gmail.com>
2023-06-25 19:21:18 +0900

    [Feat] #66 - 일기상세조회 API

---
+++ Smeem-iOS/Smeem-iOS/Network/Service/DetailDiary/DetailDiaryService.swift
@@ -0,0 +1,37 @@
+//
+//  DetailDiaryService.swift
+//  Smeem-iOS
+//
+//  Created by Joon Baek on 2023/06/25.
+//
+
+import Moya
+
+enum DetailDiaryService {
+    case detailDiary(diaryID:Int)
+}
+
+extension DetailDiaryService: BaseTargetType {
+    var path: String {
+        switch self {
+        case .detailDiary(let diaryID):
+            return "\(URLConstant.diaryURL)/\(diaryID)"
+        }
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .detailDiary:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        return NetworkConstant.tempTokenHeader
+    }
+
+}
