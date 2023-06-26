a211060e5a0ec1afd43f1a2eaa3ef3ff64d7e9cc
임주민 <limj1213@gmail.com>
2023-06-26 22:13:14 +0900

    [Feat] #70 - 홈뷰 다이어리 HomeService 네트워크 관련 파일 추가

---
+++ Smeem-iOS/Smeem-iOS/Network/Service/HomeService.swift
@@ -0,0 +1,43 @@
+//
+//  HomeService.swift
+//  Smeem-iOS
+//
+//  Created by 임주민 on 2023/06/26.
+//
+
+import Foundation
+
+import Moya
+
+enum HomeService {
+    case HomeDiary(startDate: String, endDate: String)
+}
+
+extension HomeService: BaseTargetType {
+    var path: String {
+        switch self {
+        case .HomeDiary:
+            return URLConstant.diaryURL
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .HomeDiary:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .HomeDiary(let startDate, let endDate):
+            return .requestParameters(parameters: ["start": startDate,
+                                                   "end": endDate],
+                                      encoding: URLEncoding.queryString)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return NetworkConstant.tempTokenHeader
+    }
+}
