90929c529cde50081ccb6852422d2cf6b2bcd46d
임주민 <limj1213@gmail.com>
2023-06-28 00:33:31 +0900

    [Feat] #75 - 외국어 일기 작성에서 홈뷰로 일기 작성 후 화면 연결

--- Smeem-iOS/Smeem-iOS/Global/Extensions/UIViewController+.swift
+++ Smeem-iOS/Smeem-iOS/Global/Extensions/UIViewController+.swift
@@ -89,4 +89,14 @@ extension UIViewController {
         
         return attributedString
     }
+    
+    func changeRootViewController(_ viewControllerToPresent: UIViewController) {
+         if let window = UIApplication.shared.windows.first {
+             window.rootViewController = viewControllerToPresent
+             UIView.transition(with: window, duration: 0.5, options: .transitionCrossDissolve, animations: nil)
+         } else {
+             viewControllerToPresent.modalPresentationStyle = .overFullScreen
+             self.present(viewControllerToPresent, animated: true, completion: nil)
+         }
+     }
 }
