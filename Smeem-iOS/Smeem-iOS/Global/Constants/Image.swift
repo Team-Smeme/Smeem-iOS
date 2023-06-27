50776814e5053d2808fbd4582a826f220c01a386
Joon Baek <joonbaek12@gmail.com>
2023-06-28 03:40:30 +0900

    [Add] #77 - RandomSubjectRefreshButton 이미지 에셋 추가

--- Smeem-iOS/Smeem-iOS/Global/Constants/Image.swift
+++ Smeem-iOS/Smeem-iOS/Global/Constants/Image.swift
@@ -25,6 +25,7 @@ extension Constant {
         static let icnCheckActive = UIImage(named: "icnCheckActive")
         static let icnMyPage = UIImage(named: "icnMyPage")
         static let icnRightArrow = UIImage(named: "icnRightArrow")
+        static let icnRefresh = UIImage(named: "icnRefresh")
         static let noDiary = UIImage(named: "noDiary")
         static let monthday = UIImage(named: "monthday")
         static let monthToday = UIImage(named: "monthToday")
