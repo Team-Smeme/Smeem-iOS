6de96840e3a748e68065c6b08056225b39c1f3d7
Joon Baek <joonbaek12@gmail.com>
2023-08-13 14:19:58 +0900

    [Feat] #109 - EditGoalVC 구현

---
+++ Smeem-iOS/Smeem-iOS/Presentation/MyPage/EditGoal/EditGoalViewController.swift
@@ -0,0 +1,88 @@
+//
+//  EditGoalViewController.swift
+//  Smeem-iOS
+//
+//  Created by Joon Baek on 2023/08/13.
+//
+
+import UIKit
+
+final class EditGoalViewController: UIViewController {
+    
+    private let navigationBarView = UIView()
+    
+    private lazy var backButton: UIButton = {
+        let button = UIButton()
+        button.setImage(Constant.Image.icnBack, for: .normal)
+        button.addTarget(self, action: #selector(backButtonDidTap), for: .touchUpInside)
+        return button
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "마이 페이지"
+        label.font = .s2
+        label.textColor = .smeemBlack
+        return label
+    }()
+    
+    private let howLearningView: HowLearningView = {
+        let view = HowLearningView()
+        view.buttontype = .logo
+        return view
+    }()
+    
+    private lazy var nextButton: SmeemButton = {
+        let button = SmeemButton()
+        button.smeemButtonType = .enabled
+        button.setTitle("다음", for: .normal)
+        button.addTarget(self, action: #selector(nextButtonDidTap), for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setLayout()
+    }
+
+}
+
+extension EditGoalViewController {
+
+    // MARK: - @objc
+    
+    @objc func backButtonDidTap(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func nextButtonDidTap() {
+        let mypageVC = MyPageViewController()
+        
+        self.navigationController?.pushViewController(mypageVC, animated: true)
+    }
+    
+    // MARK: - Layout
+    
+    private func setLayout() {
+        view.addSubviews(navigationBarView, howLearningView, nextButton)
+        navigationBarView.addSubviews(backButton, titleLabel)
+        
+        navigationBarView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(convertByHeightRatio(66))
+        }
+        
+        howLearningView.snp.makeConstraints {
+            $0.top.equalTo(navigationBarView.snp.bottom).offset(14)
+            $0.centerX.equalToSuperview()
+        }
+        
+        nextButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(18)
+            $0.bottom.equalToSuperview().inset(50)
+            $0.height.equalTo(convertByHeightRatio(60))
+        }
+    }
+}
