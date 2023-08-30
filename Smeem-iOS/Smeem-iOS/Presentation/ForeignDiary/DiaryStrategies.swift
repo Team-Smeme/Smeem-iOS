20c662ed466a1156af41c79b1554c420855e16a6
Joon Baek <joonbaek12@gmail.com>
2023-08-30 11:30:56 +0900

    [Chore] #122 - ConfigurePlaceholder strategy 메서드 수정

--- Smeem-iOS/Smeem-iOS/Presentation/ForeignDiary/DiaryStrategies.swift
+++ Smeem-iOS/Smeem-iOS/Presentation/ForeignDiary/DiaryStrategies.swift
@@ -14,8 +14,8 @@ final class ForeignDiaryStrategy: DiaryStrategy {
         button.setImage(Constant.Image.btnRandomSubjectInactive, for: .normal)
     }
     
-    func configurePlaceHolderLabel(_ label: UILabel) {
-        label.text = "일기를 작성해주세요."
+    func configurePlaceHolder(_ textView: UITextView) {
+        textView.text = "일기를 작성해주세요."
     }
     
     func configureToolTipView(_ imageView: UIImageView) {
@@ -50,8 +50,8 @@ final class StepOneKoreanDiaryStrategy: DiaryStrategy {
         button.setImage(Constant.Image.btnRandomSubjectInactive, for: .normal)
     }
     
-    func configurePlaceHolderLabel(_ label: UILabel) {
-        label.text = "완전한 문장으로 한국어 일기를 작성하면, 더욱 정확한\n힌트를 받을 수 있어요."
+    func configurePlaceHolder(_ textView: UITextView) {
+        textView.text = "완전한 문장으로 한국어 일기를 작성하면, 더욱 정확한\n힌트를 받을 수 있어요."
     }
     
     func configureToolTipView(_ imageView: UIImageView) {
@@ -88,8 +88,8 @@ final class StepTwoKoreanDiaryStrategy: DiaryStrategy {
         }
     }
     
-    func configurePlaceHolderLabel(_ label: UILabel) {
-        label.text = "일기를 작성해주세요."
+    func configurePlaceHolder(_ textView: UITextView) {
+        textView.text = "일기를 작성해주세요."
     }
     
     func configureToolTipView(_ imageView: UIImageView) {
