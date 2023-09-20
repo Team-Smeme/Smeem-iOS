2942e595689f45b5e7a2affc92dc21ff7ca91c65
cchanmi <113524@naver.com>
2023-09-21 01:50:50 +0900

    [Feat] #129 - base end point protocol

---
+++ Smeem-iOS/Smeem-iOS/Network/Base/BaseEndPoint.swift
@@ -0,0 +1,26 @@
+//
+//  BaseEndPoint.swift
+//  Smeem-iOS
+//
+//  Created by 황찬미 on 2023/09/21.
+//
+
+import Foundation
+
+/// end point를 작성할 때 채택해 주어야 하는 프로토콜
+
+protocol BaseEndPoint {
+    
+    var path: String { get }
+    var httpMethod: HttpMethod { get }
+    var requestBody: Data? { get }
+    var header: [String:String] { get }
+    
+    func request() -> NetworkReqeust
+}
+
+extension BaseEndPoint {
+    var baseURL: URL {
+        return URL(string: SharedConstant.baseURL)!
+    }
+}
