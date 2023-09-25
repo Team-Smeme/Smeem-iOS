68d97deb1ccc822b5811ad8e1c4eda43716f5cd7
cchanmi <113524@naver.com>
2023-09-25 15:26:14 +0900

    [Feat] #129 - requestable protocol

---
+++ Smeem-iOS/Smeem-iOS/Global/Protocols/Requestable.swift
@@ -0,0 +1,30 @@
+//
+//  Requestable.swift
+//  Smeem-iOS
+//
+//  Created by 황찬미 on 2023/09/21.
+//
+
+import Foundation
+
+protocol Requestable {
+    func request<T: Decodable>(_ request: NetworkRequest) async throws -> T?
+}
+
+final class APIService: Requestable {
+    
+    func request<T: Decodable>(_ request: NetworkRequest) async throws -> T? {
+        let (data, _) = try await URLSession.shared.data(for: request.makeUrlRequest())
+        let decoder = JSONDecoder()
+        
+        guard let decodeData = try? decoder.decode(BaseResponse<T>.self, from: data) else {
+            throw NetworkError.jsonDecodingError
+        }
+        
+        if decodeData.success {
+            return decodeData.data
+        } else {
+            throw NetworkError.clientError(message: "decode까지 성공했는데 API 결과가 false인 경우")
+        }
+    }
+}
