ef645761bb071899b641cc99e98de56c1245e061
joonBaek12 <joonbaek12@syuin.ac.kr>
2023-10-02 17:39:31 +0900

    [Add] #138 - BaseView, BaseViewController 추가

---
+++ Smeem-iOS/Smeem-iOS/Global/UIComponents/Base/BaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseViewController.swift
+//  Smeem-iOS
+//
+//  Created by Joon Baek on 2023/10/02.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        hiddenNavigationBar()
+    }
+}
