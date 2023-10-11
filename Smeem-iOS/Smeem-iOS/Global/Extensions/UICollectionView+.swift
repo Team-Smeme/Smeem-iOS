72ee810157361300abe1612aeb6368dca95c9f0c
cchanmi <113524@naver.com>
2023-10-12 01:12:30 +0900

    [Feat] #144 - extension collectionView, cell

--- Smeem-iOS/Smeem-iOS/Global/Extensions/UICollectionView+.swift
+++ Smeem-iOS/Smeem-iOS/Global/Extensions/UICollectionView+.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 extension UICollectionView {
-    func dequeueReusableCell<T: UICollectionViewCell>(cell: T.Type, indexPath: IndexPath) -> T {
-        guard let cell = self.dequeueReusableCell(withReuseIdentifier: String(describing: T.self), for: indexPath) as? T else {
-            fatalError("왜? 이거 쓰면 어케 됨?")
-        }
-        return cell
+    func registerCell<T: UICollectionViewCell>(cell: T.Type) {
+        register(cell, forCellWithReuseIdentifier: T.className)
     }
     
-    func registerCell<T: UICollectionViewCell>(cell: T.Type) {
-        register(cell, forCellWithReuseIdentifier: String(describing: T.self))
+    func dequeueReusableCell<T: UICollectionViewCell>(cell: T.Type, indexPath: IndexPath) -> T {
+        guard let cell = self.dequeueReusableCell(withReuseIdentifier: T.className, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell for type: \(T.self)")
+        }
+        return cell
     }
 }
--- Smeem-iOS/Smeem-iOS/Global/Extensions/UICollectionViewCell+.swift
+++ Smeem-iOS/Smeem-iOS/Global/Extensions/UICollectionViewCell+.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 extension UICollectionViewCell {
+    
+    static var className: String {
+        return String(describing: self)
+    }
+    
     func getIndexPath() -> IndexPath? {
         guard let superView = self.superview as? UICollectionView else { return nil }
         return superView.indexPath(for: self)
