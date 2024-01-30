48bf0f30a41ca6edf83286da4bd2534b5b120c9d
Joon Baek <joonbaek12@gmail.com>
2024-01-30 23:57:15 +0900

    [Fix] #162 - 플레이스 홀더 텍스트로 인식되는 이슈

--- Smeem-iOS/Smeem-iOS/Presentation/Diaries/Views/DiaryViewFactory.swift
+++ Smeem-iOS/Smeem-iOS/Presentation/Diaries/Views/DiaryViewFactory.swift
@@ -35,7 +35,7 @@ class DiaryViewFactory {
         let config = builder
             .setNavigationBar(navigationBarType: .diaryStepOne)
             .setTextViewType(textViewType: .editable(textViewHandler))
-            .setPlaceholderText(placeholderText: "완벽한 문장으로 한국어 일기를 작성하면, 더욱 정확한 힌트를 받을 수 있어요.")
+            .setPlaceholderText(placeholderText: "완전한 문장으로 한국어 일기를 작성하면, 더욱 정확한 힌트를 받을 수 있어요.")
             .setBottomViewType(bottomViewType: .standard)
             .build()
 
