1e1c0e38f526cca127505c80461a53d9c7eebf44
cchanmi <113524@naver.com>
2024-07-30 18:13:22 +0900

    [Feat] #212 - keyboard 높이에 따라 view 위치 이동

--- Smeem-iOS/Smeem-iOS/Presentation/ResignSummray/ResignSummaryViewController.swift
+++ Smeem-iOS/Smeem-iOS/Presentation/ResignSummray/ResignSummaryViewController.swift
@@ -8,13 +8,15 @@
 import UIKit
 import Combine
 
-final class ResignSummaryViewController: BaseViewController {
+final class ResignSummaryViewController: BaseViewController, UITextViewDelegate {
     
     // MARK: Publisher
     
     private let viewWillAppearSubject = PassthroughSubject<Void, Never>()
     private let cellTapped = PassthroughSubject<Int, Never>()
     private let nextButtonTapped = PassthroughSubject<Void, Never>()
+    private let keyboardSubject = PassthroughSubject<KeyboardType, Never>()
+    private let keyboardHeightSubject = PassthroughSubject<KeyboardInfo, Never>()
     private var cancelBag = Set<AnyCancellable>()
     
     private let viewModel = ResignSummaryViewModel()
@@ -73,13 +75,16 @@ final class ResignSummaryViewController: BaseViewController {
         label.text = "계정 삭제 사유를 자유롭게 적어주세요."
         label.font = .b2
         label.textColor = .smeemBlack
-//        label.setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
         return label
     }()
     
     private let summaryTextView: UITextView = {
         let textView = UITextView()
+        textView.text = "계정 삭제 사유를 적어주세요."
+        textView.textColor = .gray400
+        textView.font = .b4
         textView.backgroundColor = .gray100
+        textView.contentInset = UIEdgeInsets(top: 11, left: 11, bottom: 11, right: 11)
         return textView
     }()
     
@@ -95,6 +100,7 @@ final class ResignSummaryViewController: BaseViewController {
         
         setLayout()
         bind()
+        setDelegate()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -103,18 +109,87 @@ final class ResignSummaryViewController: BaseViewController {
         self.viewWillAppearSubject.send(())
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.summaryTextView.resignFirstResponder()
+    }
+    
     // MARK: - Method
     
     private func bind() {
+        NotificationCenter.default.publisher(for: UITextView.textDidBeginEditingNotification, object: summaryTextView)
+            .sink { _ in
+//                self.keyboardSubject.send(.up)
+            }
+            .store(in: &cancelBag)
+        
+        NotificationCenter.default.publisher(for: UITextView.textDidEndEditingNotification, object: summaryTextView)
+            .sink { [weak self] _ in
+                self?.keyboardHeightSubject.send(KeyboardInfo(type: .down, keyboardHeight: nil, viewHeight: nil))
+            }
+            .store(in: &cancelBag)
+        
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)
+            .compactMap { $0.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue }
+            .map { $0.cgRectValue.height }
+            .sink { [weak self] keyboardHeight in
+                self?.keyboardHeightSubject.send(KeyboardInfo(type: .up,
+                                                              keyboardHeight: keyboardHeight,
+                                                              viewHeight: self!.view.frame.origin.y))
+            }
+            .store(in: &cancelBag)
+        
         let output = viewModel.transform(input: ResignSummaryViewModel.Input(viewWillAppearSubject: viewWillAppearSubject,
                                                                             cellTapped: cellTapped,
-                                                                            buttonTapped: nextButtonTapped))
+                                                                            buttonTapped: nextButtonTapped,
+                                                                            keyboardSubject: keyboardSubject,
+                                                                            keyboardHeightSubject: keyboardHeightSubject))
         output.viewWillAppearResult
             .sink { array in
                 self.trainingGoalCollectionView.planGoalArray = array
                 self.trainingGoalCollectionView.reloadData()
             }
             .store(in: &cancelBag)
+        
+        output.cellResult
+            .sink { [weak self] string in
+                print(string)
+            }
+            .store(in: &cancelBag)
+        
+//        output.buttonResult
+//            .sink { [weak self] type in
+//                self?.resignButton.changeButtonType(buttonType: type)
+//            }
+//            .store(in: &cancelBag)
+        
+        output.enabledButtonResult
+            .sink { [weak self] type in
+                self?.resignButton.changeButtonType(buttonType: type)
+            }
+            .store(in: &cancelBag)
+        
+        output.notEnabledButtonResult
+            .sink { [weak self] type in
+                self?.resignButton.changeButtonType(buttonType: type)
+                self?.summaryTextView.becomeFirstResponder()
+            }
+            .store(in: &cancelBag)
+        
+        let viewY = self.view.safeAreaLayoutGuide.snp.height
+        print(viewY)
+        
+        output.keyboardResult
+            .sink { [weak self] keyboardValue in
+                UIView.animate(withDuration : 0.3) {
+                    self?.view.frame.origin.y = keyboardValue
+                }
+            }
+            .store(in: &cancelBag)
+    }
+    
+    private func setDelegate() {
+        trainingGoalCollectionView.resignSummaryDelegate = self
+        self.summaryTextView.delegate = self
     }
     
     private func setLayout() {
--- Smeem-iOS/Smeem-iOS/Presentation/ResignSummray/ResignSummaryViewModel.swift
+++ Smeem-iOS/Smeem-iOS/Presentation/ResignSummray/ResignSummaryViewModel.swift
@@ -8,6 +8,17 @@
 import Foundation
 import Combine
 
+struct KeyboardInfo {
+    let type: KeyboardType
+    let keyboardHeight: CGFloat?
+    let viewHeight: CGFloat?
+}
+
+enum KeyboardType {
+    case up
+    case down
+}
+
 final class ResignSummaryViewModel: ViewModel {
     
     private let summaryData: [Goal] = [Goal(goalType: nil, name: "이용이 어렵고 서비스가 불안정해요."),
@@ -20,12 +31,25 @@ final class ResignSummaryViewModel: ViewModel {
         let viewWillAppearSubject: PassthroughSubject<Void, Never>
         let cellTapped: PassthroughSubject<Int, Never>
         let buttonTapped: PassthroughSubject<Void, Never>
+        let keyboardSubject: PassthroughSubject<KeyboardType, Never>
+        let keyboardHeightSubject: PassthroughSubject<KeyboardInfo, Never>
     }
     
     struct Output {
         let viewWillAppearResult: AnyPublisher<[Goal], Never>
         let cellResult: AnyPublisher<String, Never>
+        let buttonResult: AnyPublisher<SmeemButtonType, Never>
+        let keyboardResult: AnyPublisher<CGFloat, Never>
+        let enabledButtonResult: PassthroughSubject<SmeemButtonType, Never>
+        let notEnabledButtonResult: PassthroughSubject<SmeemButtonType, Never>
     }
+    
+    private let cellIndexSubject = PassthroughSubject<Int, Never>()
+    private let enabledButtonResult = PassthroughSubject<SmeemButtonType, Never>()
+    private let notEnabledButtonResult = PassthroughSubject<SmeemButtonType, Never>()
+    private var keyboardHeight = 0.0
+    private var cancelBag = Set<AnyCancellable>()
+    private var totalViewHeight: CGFloat = 0.0
     
     func transform(input: Input) -> Output {
         let viewWillAppearResult = input.viewWillAppearSubject
@@ -36,11 +60,36 @@ final class ResignSummaryViewModel: ViewModel {
         
         let cellResult = input.cellTapped
             .map { index -> String in
+                self.cellIndexSubject.send(index)
                 return self.summaryData[index].name
+            }
+            .eraseToAnyPublisher()
+        
+        let buttonResult = cellIndexSubject
+            .map { index -> SmeemButtonType in
+                return index == 4 ? .notEnabled : .enabled
+            }
+            .eraseToAnyPublisher()
+        
+        cellIndexSubject
+            .sink { index -> Void in
+                if index == 4 { self.notEnabledButtonResult.send(.notEnabled) }
+                else { self.enabledButtonResult.send(.enabled) }
+            }
+            .store(in: &cancelBag)
+        
+        let keyboardResult = input.keyboardHeightSubject
+            .map { info -> CGFloat in
+                if let viewHeight = info.viewHeight { self.totalViewHeight = viewHeight }
+                return info.type == .up ? self.totalViewHeight-info.keyboardHeight! : self.totalViewHeight
             }
             .eraseToAnyPublisher()
             
         return Output(viewWillAppearResult: viewWillAppearResult,
-                      cellResult: cellResult)
+                      cellResult: cellResult,
+                      buttonResult: buttonResult,
+                      keyboardResult: keyboardResult,
+                      enabledButtonResult: enabledButtonResult,
+                      notEnabledButtonResult: notEnabledButtonResult)
     }
 }
