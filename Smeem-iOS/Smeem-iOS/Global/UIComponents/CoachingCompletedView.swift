520b76f5d553137f1f4b8cee6084689775c839ec
Joon Baek <joonbaek12@gmail.com>
2024-10-23 00:06:08 +0900

    [Feat] #220 - 코칭뷰 UI 구현

---
+++ Smeem-iOS/Smeem-iOS/Global/UIComponents/CoachingCompletedView.swift
@@ -0,0 +1,46 @@
+//
+//  CoachingCompletedView.swift
+//  Smeem-iOS
+//
+//  Created by Joon Baek on 2024/10/16.
+//
+
+import SwiftUI
+
+struct CoachingCompletedView: View {
+    var body: some View {
+        
+        SwiftUINavigationView()
+        
+        VStack() {
+            Text(MockData.headerText)
+              .font(Font.custom("Pretendard", size: 16))
+            // Colors 상수 등록 필요
+              .foregroundColor(Color(UIColor.smeemBlack))
+              .frame(maxWidth: .infinity, alignment: .leading)
+            
+            ScrollView {
+                Text(MockData.diaryEntry)
+                  .font(Font.custom("Pretendard", size: 16))
+                  .foregroundColor(Color(UIColor.gray400))
+                  .lineSpacing(0.375)
+            }
+            .frame(height: screenHeight * 0.32)
+            
+            Spacer()
+        }
+        .padding(.horizontal, screenWidth * 0.048)
+    }
+}
+
+extension CoachingCompletedView {
+    struct MockData {
+        static let headerText = "일기를 잘 작성하셨어요! \n내용이 명확하고 흥미로웠습니다.\n이제 몇 가지 문법적 오류를 수정해 볼까요?"
+        static let diaryEntry = "I watched Avatar with my boyfriend at Hongdae CGV. I should have skimmed the previous season   what they were saying and the universe(??). What I was annoyed then was 두팔 didn’t know that as me. I think 두팔 who is my boyfriend should study before wathcing…. but Avatar2 is amazing movie I think. In my personal opinion, the jjin main character of Avatar2 is not Sully, but his son.the jjin main character of Avatar2 is not Sully, but his son.the jjin main character of Avatar2 is not Sully, but his son.the jjin main character of Avatar2 is not Sully, but his son.character of Avatar2 is not Sully, but his son.ly, but his ly, but his ly, but 여기부터 스크롤영역이라 내려가면 글자가 계속 나올거에욤ㄴㅇㄹ리ㅏㄴㅇ리ㅏㅓㄴ이ㅏ러민아ㅓ리ㅏㄴㅁ어리"
+    }
+}
+
+@available (iOS 17, *)
+#Preview {
+    CoachingCompletedView()
+}
