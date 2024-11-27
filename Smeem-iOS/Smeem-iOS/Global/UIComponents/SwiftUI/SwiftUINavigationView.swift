eadb4bcc22c8ccf5bd21fca26862c2cd025b3e0a
Joon Baek <joonbaek12@gmail.com>
2024-11-27 10:30:23 +0900

    [Chore] #222 - 네비바 수정

--- Smeem-iOS/Smeem-iOS/Global/UIComponents/SwiftUI/SwiftUINavigationView.swift
+++ Smeem-iOS/Smeem-iOS/Global/UIComponents/SwiftUI/SwiftUINavigationView.swift
@@ -14,35 +14,55 @@ enum NavigationbarType {
 
 struct SwiftUINavigationView: View {
     let navigationbarType: NavigationbarType
+    @State private var showFloatingView = false
     @Binding var selectedIndex: Int
     
     let options = ["코칭 OFF", "코칭 ON"]
     
     var body: some View {
         HStack {
-            Button(action: {
-                // 뒤로가기 액션
-            }, label: {
-                Image("icnBack")
-                    .imageScale(.large)
-            })
-            .padding(.leading, 10)
+            // 뒤로가기 버튼
+            if navigationbarType == .diaryDetails {
+                Button(action: {
+                    // 뒤로가기 액션
+                }, label: {
+                    Image("icnBack")
+                        .imageScale(.large)
+                })
+                .padding(.leading, 10 / screenWidth)
+            } else {
+                Spacer().frame(width: 30 / screenWidth)
+            }
             
+            // 중앙 콘텐츠 (CustomSegmentedControl)
             if navigationbarType == .diaryDetails {
                 CustomSegmentedControl(selectedIndex: $selectedIndex, options: options)
-                    .frame(height: 32)
+                    .frame(height: 32 / screenHeight)
                     .padding(65)
             }
             
             Spacer()
             
-            Button(action: {
-                // 닫기 버튼 액션
-            }, label: {
-                Text("닫기")
-                    .tint(.black)
-            })
-            .padding(.trailing, 18)
+            // 오른쪽 버튼
+            if navigationbarType == .diaryDetails {
+                Button(action: {
+                    showFloatingView = true
+                }, label: {
+                    Image("icnMore")
+                })
+                .padding(.trailing, 18 / screenWidth)
+                .fullScreenCover(isPresented: $showFloatingView) {
+                    FloatingButtonsSwiftUIView()
+                }
+            } else {
+                Button(action: {
+                    // 다른 버튼 액션
+                }, label: {
+                    Text("닫기")
+                        .tint(.black)
+                })
+                .padding(.trailing, 18 / screenWidth)
+            }
         }
         .frame(height: screenHeight * (66 / screenHeight))
     }
